564c4eca360b55e98753dda6596559ac4196a0dd
zhd <709688530@qq.com>
2020-09-18 17:54:36 +0800

    111

---
+++ src/main/java/com/exmaple/stream/day01/Test.java
@@ -0,0 +1,30 @@
+package com.exmaple.stream.day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("欧阳雪", 18, "中国", 'F'));
+        personList.add(new Person("Tom", 24, "美国", 'M'));
+        personList.add(new Person("Harley", 22, "英国", 'F'));
+        personList.add(new Person("向天笑", 20, "中国", 'M'));
+        personList.add(new Person("李康", 22, "中国", 'M'));
+        personList.add(new Person("小梅", 20, "中国", 'F'));
+        personList.add(new Person("何雪", 21, "中国", 'F'));
+        personList.add(new Person("李康", 22, "中国", 'M'));
+
+
+        personList.stream().filter((p)->p.getAge()>18).forEach(System.out::println);
+
+        System.out.println("-------------------------");
+
+        long count = personList.stream().filter((p) -> p.getCountry().equals("中国")).count();
+        System.out.println("中国人有："+count);
+
+    }
+
+
+}
